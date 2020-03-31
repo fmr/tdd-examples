75315f8e7d809791327b9d7e9d176f7c6f0dff49
Dean Budd <dbudd@seek.com.au>
2020-03-31 15:45:41 +1100

    minor change to reference data

--- src/data.js
+++ src/data.js
@@ -1,15 +1,15 @@
 const exampleInput = {
   roleRequirements: {
-    "question-6": ["I'm an Australian citizen"],
-    "question-14": ["Tuesday 8am-6pm", "Thursday 8am-6pm"],
+    "question-1": ["I'm an Australian citizen"],
+    "question-2": ["Tuesday 8am-6pm", "Thursday 8am-6pm"],
     "question-910de548-8616-4b37-b6dc-10a63f08a05c": "some expo testing",
   },
 };
 
 const exampleOutput = {
   questionnaire: [
-    { questionId: "6", answers: ["I'm an Australian citizen"] },
-    { questionId: "14", answers: ["Tuesday 8am-6pm", "Thursday 8am-6pm"] },
+    { questionId: "1", answers: ["I'm an Australian citizen"] },
+    { questionId: "2", answers: ["Tuesday 8am-6pm", "Thursday 8am-6pm"] },
     {
       questionId: "910de548-8616-4b37-b6dc-10a63f08a05c",
       answers: ["some expo testing"],
